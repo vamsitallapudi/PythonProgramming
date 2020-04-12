bbdb141bd50d9f09298025dea409d1192a468f35
Vamsi Tallapudi <vamsi@coderefer.com>
2020-04-12 12:08:56 +0530

    diameter of binary tree solution

---
+++ leetcode/30_day_challenge/DiameterOfBinaryTree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        self.globalLongestPath = 0
+        self.depth(root)
+        return self.globalLongestPath
+
+    def depth(self, node:TreeNode)-> int:
+        if not node:
+            return 0
+        left = self.depth(node.left)
+        right = self.depth(node.right)
+        self.globalLongestPath = max(self.globalLongestPath, left + right)
+        return max(left,right)+1
+
+
+Solution.diameterOfBinaryTree([1,2,3,4])
+
+
