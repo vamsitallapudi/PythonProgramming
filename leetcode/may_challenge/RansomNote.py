dd8f1e84ad2e11493ad9e358d575eda80ff55ac9
Vamsi Tallapudi <vamsi@coderefer.com>
2020-05-05 09:18:43 +0530

    ransom note modified answer - single liner with time complexity of O(m+n)

--- leetcode/may_challenge/RansomNote.py
+++ leetcode/may_challenge/RansomNote.py
@@ -1,22 +1,9 @@
+import collections
+
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        magSet, ranSet = {}, {}
-
-        def helper(str, my_set):
-            for i in str:
-                if i not in my_set:
-                    my_set[i] = 1
-                else:
-                    my_set[i] += 1
-
-        helper(magazine, magSet)
-        helper(ransomNote, ranSet)
-        for i in ranSet.keys():
-            if magSet.get(i) is not None and magSet.get(i) >= ranSet.get(i):
-                continue
-            else:
-                return False
-        return True
+        return not collections.Counter(ransomNote) - collections.Counter(magazine)
 
 
-print(Solution().canConstruct("ae", "adca"))
+print(Solution().canConstruct("aad", "adca"))
