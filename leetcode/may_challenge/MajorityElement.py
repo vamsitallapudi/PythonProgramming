65a742f96156d3c92e1bb65c8d667eccea9b5ee1
Vamsi Tallapudi <vamsi@coderefer.com>
2020-05-07 10:13:11 +0530

    majority element editorial simplified

--- leetcode/may_challenge/MajorityElement.py
+++ leetcode/may_challenge/MajorityElement.py
@@ -4,16 +4,8 @@ import math
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        eleMap = {}
-        majority = nums[0]
-        for i in nums:
-            if eleMap.get(i) is None:
-                eleMap[i] = 1
-            else:
-                eleMap[i] += 1
-            if eleMap[i] >= math.ceil(len(nums)/2):
-                majority = i
-        return majority
+        nums.sort()
+        return nums[len(nums)//2]
 
 
 print(Solution().majorityElement([6,6,6,7,7]))
