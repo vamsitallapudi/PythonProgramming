41df1be8e492ec78e216f0d293e446ed22cd8bd4
Vamsi Tallapudi <vamsi@coderefer.com>
2020-05-10 19:43:58 +0530

    majority element modified single liner

--- leetcode/may_challenge/MajorityElement.py
+++ leetcode/may_challenge/MajorityElement.py
@@ -4,8 +4,7 @@ import math
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+        return sorted(nums)[len(nums)//2]
 
 
 print(Solution().majorityElement([6,6,6,7,7]))
