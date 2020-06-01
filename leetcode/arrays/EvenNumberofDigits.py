d4f7f759434670b68a22e02723623599607da2d3
Vamsi Tallapudi <vamsi@coderefer.com>
2020-06-01 12:12:44 +0530

    even number of digits

--- leetcode/arrays/EvenNumberofDigits.py
+++ leetcode/arrays/EvenNumberofDigits.py
@@ -3,20 +3,4 @@ from typing import List
 
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
-        count = 0
-        for i in nums:
-            if self.is_even(i):
-                count +=1
-        return count
-
-    def is_even(self, i) -> bool:
-        count = 1
-        if i < 10:
-            return False
-        while i > 10:
-            i /= 10
-            count += 1
-        if count % 2 == 0:
-            return True
-        else:
-            return False
+        return sum(len(str(i)) % 2 == 0 for i in nums)
