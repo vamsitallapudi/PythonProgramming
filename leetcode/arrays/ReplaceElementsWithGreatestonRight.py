7f7ae453878fdedc6ad960179b545f588b41e441
Vamsi Tallapudi <vamsi@coderefer.com>
2020-06-05 15:05:54 +0530

    replace elements - with sleek solution in python

--- leetcode/arrays/ReplaceElementsWithGreatestonRight.py
+++ leetcode/arrays/ReplaceElementsWithGreatestonRight.py
@@ -5,9 +5,7 @@ class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
         maxVal = -1
         for i in range(len(arr) - 1, -1, -1):
-            temp = arr[i]
-            arr[i] = maxVal
-            maxVal = max(maxVal, temp)
+            arr[i], maxVal = maxVal, max(maxVal, arr[i])
         return arr
 
 
