2e71ceacd972028da435b299e277b36ab5ee15f5
Vamsi Tallapudi <vamsi@coderefer.com>
2020-07-01 08:50:17 +0530

    modifying base linked list to accommodate more common functionality

---
+++ dsame/linkedLists/problems/ReversingLinkedList.py
@@ -0,0 +1,21 @@
+from dsame.linkedLists.problems.BaseLinkedList import BaseLinkedList
+
+
+def reverse_linked_list(head):
+    # iterative version
+    temp, next = None, None
+    while head:
+        next = head.next
+        head.next = temp
+        temp = head
+        head = next
+
+    return temp
+
+
+if __name__ == '__main__':
+    bll = BaseLinkedList()
+    head = bll.initializebll(bll)
+    head = reverse_linked_list(head)
+    bll.print_data(head)
+
