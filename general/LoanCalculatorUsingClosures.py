4d414f8c054b793546f51b849401e886d3878384
Vamsi Tallapudi <vamsi@coderefer.com>
2020-09-28 12:27:26 +0530

    comments added

--- general/LoanCalculatorUsingClosures.py
+++ general/LoanCalculatorUsingClosures.py
@@ -9,5 +9,5 @@ principle = 100000
 home_loan = interest(7, principle)  # percentage of 7
 personal_loan = interest(11, principle)  # percentage of 11
 
-print(home_loan(20))
-print(personal_loan(3))
+print(home_loan(20))  # for 20 years
+print(personal_loan(3))  # for 3 years
