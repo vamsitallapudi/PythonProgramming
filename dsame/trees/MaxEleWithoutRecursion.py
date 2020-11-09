87d615bb08a064e7412f7be1df277c2086aa9ec9
Vamsi Tallapudi <vamsi@coderefer.com>
2020-11-09 18:30:23 +0530

    maximum element in a binary tree iterative - repeat again

--- dsame/trees/MaxEleWithoutRecursion.py
+++ dsame/trees/MaxEleWithoutRecursion.py
@@ -1,26 +1,25 @@
-from dsame.trees.BinaryTreeNode import BinaryTreeNode
+from dsame.trees.BinaryTreeNode import *
 
 
 # using level order
 
-def findMax(root: BinaryTreeNode):
-    values = []
+def findMax(root: BinaryTreeNode) -> BinaryTreeNode:
+    if not root:
+        return root
+    stack = [root]
     max = 0
-    values.append(root)
 
-    while len(values):
-        temp = values.pop()
+    while len(stack):
+        temp = stack.pop()
         if max < temp.data:
+            # process
             max = temp.data
         if temp.left:
-            values.append(temp.left)
+            stack.append(temp.left)
         if temp.right:
-            values.append(temp.right)
+            stack.append(temp.right)
 
     return max
 
 
-a = BinaryTreeNode(5)
-b = BinaryTreeNode(2)
-c = BinaryTreeNode(3, a, b)
-print(findMax(c))
+print(findMax(initializeBinaryTree()))
