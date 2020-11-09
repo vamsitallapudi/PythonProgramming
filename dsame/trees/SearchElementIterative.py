c2e95398914589eeb1523da64d64e17b0a0f3661
Vamsi Tallapudi <vamsi@coderefer.com>
2020-11-09 19:13:12 +0530

    search element iteratively - repeat

--- dsame/trees/SearchElementIterative.py
+++ dsame/trees/SearchElementIterative.py
@@ -1,25 +1,21 @@
-from dsame.trees.BinaryTreeNode import BinaryTreeNode
+from dsame.trees.BinaryTreeNode import *
 
 
 def search_ele_iterative(ele, root: BinaryTreeNode):
     if not root:
-        return -1
+        return False
+    stack = [root]
 
-    values = [root]
-    while len(values):
-        temp = values.pop()
-        if ele == temp.data:
+    while len(stack) > 0:
+        temp = stack.pop()
+        if temp.data == ele:
             return True
         if temp.left:
-            values.append(temp.left)
+            stack.append(temp.left)
         if temp.right:
-            values.append(temp.right)
+            stack.append(temp.right)
 
     return False
 
 
-a = BinaryTreeNode(5)
-b = BinaryTreeNode(2)
-c = BinaryTreeNode(3, a, b)
-print(search_ele_iterative(4, c))
-print(search_ele_iterative(5, c))
+print(search_ele_iterative(4, initializeBinaryTree()))
