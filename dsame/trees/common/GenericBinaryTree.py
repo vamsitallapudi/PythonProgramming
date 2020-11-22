97d8ec07d7419137c5acfbc4307ad2997aa4d469
Vamsi Tallapudi <vamsi@coderefer.com>
2020-11-22 17:04:22 +0530

    additional methods for GenericBinaryTree.py to calculate siblings

--- dsame/trees/common/GenericBinaryTree.py
+++ dsame/trees/common/GenericBinaryTree.py
@@ -7,7 +7,16 @@ class GenericTreeNode:
 
 def initializeGenericTree() -> GenericTreeNode:
     c = GenericTreeNode(4)
-    a = GenericTreeNode(3, firstChild=c)
+    e = GenericTreeNode(5)
+    a = GenericTreeNode(3, firstChild=c, nextSibling=e)
     b = GenericTreeNode(2, nextSibling=a)
     d = GenericTreeNode(1, firstChild=b)
     return d
+
+
+def initializeGenericTreeForSiblings() -> GenericTreeNode:
+    c = GenericTreeNode(4)
+    d = GenericTreeNode(5)
+    a = GenericTreeNode(3, firstChild=c, nextSibling=d)
+    b = GenericTreeNode(2, nextSibling=a)
+    return b
