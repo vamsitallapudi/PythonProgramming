81b90c677986e44c8e54c2bbc41e889aaa2c9b9e
Vamsi Tallapudi <vamsi@coderefer.com>
2020-12-06 10:27:59 +0530

    max number of k sum pairs

--- leetcode/contests/weekly/MaxNoOfKSumPairs5618.py
+++ leetcode/contests/weekly/MaxNoOfKSumPairs5618.py
@@ -1,16 +1,21 @@
 from typing import List
 
 
+# using 2 pointer solution
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
+        i, j = 0, len(nums) - 1
         count = 0
-        for i in list(nums):
-            remaining = k - i
-            if remaining in nums:
+        while i < j:
+            total = nums[i] + nums[j]
+            if total == k:
+                i += 1
+                j -= 1
                 count += 1
-                nums.remove(i)
-                nums.remove(remaining)
-        return count
+            elif total < k:
+                i += 1
+            else:
+                j += 1
 
 
 print(Solution().maxOperations([3, 1, 3, 4, 3], 6))
