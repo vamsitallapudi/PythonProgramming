108342f994eee93827cb08568d046139b01fcef2
Vamsi Tallapudi <vamsi@coderefer.com>
2020-12-06 13:32:00 +0530

    max number of k-sum pairs leetcode 1679th problem

---
+++ dsame/trees/binary_search_trees/FindLCA.py
@@ -0,0 +1,3 @@
+from dsame.trees.common.BinarySearchTreeNode import *
+
+# given a BST find LCA 
--- leetcode/contests/weekly/MaxNoOfKSumPairs5618.py
+++ leetcode/contests/weekly/MaxNoOfKSumPairs5618.py
@@ -1,21 +1,21 @@
 from typing import List
 
 
-# using 2 pointer solution
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
         i, j = 0, len(nums) - 1
         count = 0
+
         while i < j:
             total = nums[i] + nums[j]
+
             if total == k:
+                count += 1
                 i += 1
                 j -= 1
-                count += 1
-            elif total < k:
-                i += 1
+            elif total > k:
+                j -= 1
             else:
-                j += 1
-
-
-print(Solution().maxOperations([3, 1, 3, 4, 3], 6))
+                i += 1
+        return count
